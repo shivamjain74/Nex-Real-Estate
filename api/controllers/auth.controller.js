d7e80e88de0d76d0d085e06391547cab1be7a123
Shivam Jain <shivamjain7744@gmail.com>
2024-05-05 19:56:15 +0530

    Added cors origin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,29 +37,26 @@ export const signin = async(req,res,next)=>{
 
 export const google = async (req,res,next)=>{
     try {
-        const user = await User.findOne({email: req.body.email})
-        if(user){
-             const token =jwt.sign({ id :user._id},process.env.JWT_SECRET);
+        const user = await User.findOne({email: req.body.email}) // yaha check krega ki hai bhi ki nhiii
+        if(user){ // checking user if exist or not
+             const token =jwt.sign({ id :user._id},process.env.JWT_SECRET); // token for registering
              const {password: pass, ...rest } = user._doc;
              res
-             .cookie('access_token',token,{httpOnly:true})
+             .cookie('access_token',token,{httpOnly:true}) // cookie 
              .status(200)
              .json(rest);
         }
-        else{
+        else{ // password generate kiya hai kyuki signin krte time password nhi mang raha google isliye ider create kr rahe h
 
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-            const hashedPassword =  bcryptjs.hashSync(generatedPassword,10);
+            const hashedPassword =  bcryptjs.hashSync(generatedPassword,10); // hashing the password by bcryptjs
             const newUser= new User ({username:req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4) , email:req.body.email, password: hashedPassword, avatar: req.body.photo });
-            await newUser.save();
-            const token= jwt.sign({id: newUser._id},process.env.JWT_SECRET);
+            await newUser.save(); //ider phele save kr liye 
+            const token= jwt.sign({id: newUser._id},process.env.JWT_SECRET); // phir whi token generate kiya
             const {password: pass, ...rest }= newUser._doc;
             res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
 
         }
-
-
-
     } catch (error) {
         next(error)
     }
